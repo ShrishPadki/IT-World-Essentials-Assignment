965c03f77fd74dd4418b435cdf21cdbabd470cd4
ShrishPadki <shrishshrivatsa0@gmail.com>
2026-03-31 10:14:58 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+float simpleInterest(float P, float R, float T) { 
+return (P * R * T) / 100; 
+} 
+int main() { 
+float P, R, T, SI; 
+printf("Enter Principal: "); 
+scanf("%f", &P); 
+printf("Enter Rate: "); 
+scanf("%f", &R); 
+printf("Enter Time: "); 
+scanf("%f", &T); 
+SI = simpleInterest(P, R, T); 
+printf("Simple Interest = %.2f\n", SI); 
+return 0; 
+} 
